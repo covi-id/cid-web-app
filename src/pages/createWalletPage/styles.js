179ae0991ef103f509db0171707cd4c4ebcb6ca4
MDRKeil <matt@thedelta.io>
2020-04-21 13:18:40 +0200

    Fixed media queries

--- src/pages/createWalletPage/styles.js
+++ src/pages/createWalletPage/styles.js
@@ -6,8 +6,14 @@ export const BackTick = styled.img.attrs({ src: Back })`
   width: 50px;
   margin: 0;
   padding: 0;
-  padding-bottom: 14px;
+  padding-top: 6px;
   padding-right: 10px;
+
+  @media screen and (max-width: 600px) {
+    padding-bottom: 27px;
+    padding-right: 3px;
+    width: 33px;
+  }
 `
 export const BackButton = styled.button`
   text-decoration: none;
@@ -40,26 +46,6 @@ export const Container = styled.div`
   }
 `
 
-export const Heading = styled.div`
-  width: 100%;
-  height: 100%;
-  margin-bottom: 14px;
-  font-size: 3rem;
-  font-weight: bold;
-  color: #654cf0;
-`
-
-export const Text = styled.div`
-  width: 160px;
-  height: 20px;
-  vertical-align: middle;
-  text-align: center;
-  font-size: 1.06rem;
-  letter-spacing: 0.01em;
-  font-weight: bold;
-  color: #654cf0;
-`
-
 export const StepIndicator = styled.div`
   padding: 0 20px;
   height: 35px;
@@ -79,7 +65,7 @@ export const StepText = styled.p`
   margin: auto;
   color: #c9c0fa;
   font-weight: 600;
-  font-size: 1rem;
+  font-size: 17px;
 `
 
 export const HeadingContainer = styled.div`
@@ -88,13 +74,12 @@ export const HeadingContainer = styled.div`
   flex-direction: column;
 `
 export const StepHeading = styled.h1`
-  font-size: 2.9rem;
+  font-size: 48px;
   font-weight: bold;
   color: #654cf0;
-  margin: 10px 0 30px 0;
 
   @media screen and (max-width: 600px) {
-    font-size: 2.5rem;
+    font-size: 28px;
     margin: 2.42% 0 12.36% 0;
   }
 `
